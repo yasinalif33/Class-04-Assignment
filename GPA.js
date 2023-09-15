08681e535971ab72fe166092cdc4c113725ff6af
Yasin Arafat Alif <70240790+yasinalif33@users.noreply.github.com>
2023-09-16 03:52:09 +0600

    Add files via upload

---
+++ GPA.js
@@ -0,0 +1,18 @@
+var mark = 55;
+switch(true){
+//     case(mark<=100):console.log('A+');
+// break;
+    case(mark>=91 && mark<=100):console.log('A+');
+break;
+    case(mark>=81 && mark<=90):console.log('A');
+break;
+    case(mark>=71 && mark<=80):console.log('B');
+break;
+    case(mark>=61 && mark<=70):console.log('C');
+break;
+    case(mark>=51 && mark<=60):console.log('D');
+break;
+    case(mark>=41 && mark<=50):console.log('E');
+break;
+default:console.log("You're failed.");
+}
---
+++ break.js
@@ -0,0 +1,8 @@
+let x = 1;
+while(x<10){
+    if (x = 5){
+        break;
+    }
+    x = x + 1;
+}
+console.log(x);
